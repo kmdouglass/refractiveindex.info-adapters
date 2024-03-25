15d623c4d0152bb5243e866e7446eb14c6141507
Kyle Douglass <kyle.douglass@epfl.ch>
2024-03-25 14:36:31 +0100

    Full conversion working; need to handle numbers as page names

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,13 +1,23 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
 #[clap(version, author)]
 pub struct Args {
-    /// A refractiveindex.info catalog file
-    #[arg(short, long, value_name = "FILE", default_value = "./catalog-nk.yml")]
-    pub input: std::path::PathBuf,
+    /// The path to the refractiveindex.info database folders
+    #[arg(short, long, value_name = "PATH", default_value = "./database")]
+    pub path: std::path::PathBuf,
+
+    /// The catalog to parse
+    #[arg(short, long, value_name = "TYPE", default_value = "nk")]
+    pub catalog: Catalog,
 
     /// The file to write the parsed results to
     #[arg(short, long, value_name = "FILE", default_value = "./results.json")]
     pub output: std::path::PathBuf,
+}
+
+#[derive(ValueEnum, Debug, Clone)]
+pub enum Catalog {
+    N2,
+    NK,
 }
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,44 @@ use anyhow::Result;
 
 use clap::Parser;
 
-use lib_ria::cli::Args;
+use lib_ria::cli::{Args, Catalog as CatalogChoice};
 use lib_ria::database::Catalog;
 use lib_ria::Store;
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    println!("{:?}", args);
+    // Save the current directory
+    let current_dir = std::env::current_dir()?;
+
+    // Change the current directory to the database path
+    std::env::set_current_dir(&args.path)?;
 
     // Open the file specified in the args
-    let file = std::fs::File::open(&args.input)?;
+    let file = match &args.catalog {
+        CatalogChoice::N2 => std::fs::File::open("catalog-n2.yml")?,
+        CatalogChoice::NK => std::fs::File::open("catalog-nk.yml")?,
+    };
     let reader = std::io::BufReader::new(file);
 
-    // Parse the YAML file into the database's internal representation
+    // Deserialize the catalog YAML file
     let catalog: Catalog = serde_yaml::from_reader(reader)?;
+
+    // Parse the catalog into this library's internal representation
+    match &args.catalog {
+        CatalogChoice::N2 => {
+            std::env::set_current_dir("data-n2")?;
+        }
+        CatalogChoice::NK => {
+            std::env::set_current_dir("data-nk")?;
+        }
+    }
     let store = Store::try_from(catalog)?;
 
     // Write the store to the output file in JSON format
+    std::env::set_current_dir(current_dir)?;
     let file = std::fs::File::create(&args.output)?;
     let writer = std::io::BufWriter::new(file);
-
     serde_json::to_writer_pretty(writer, &store)?;
 
     Ok(())
